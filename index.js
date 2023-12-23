31cc6e5423338fb487b8e9d0f4b2cf8459d79e43
okHassan <freelancerhassan3@gmail.com>
2023-12-23 23:50:50 +0600

    dev-10

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static('build'));
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+
+// make a test route
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')  
+})
 
 const userSocketMap = {};
 
